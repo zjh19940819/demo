32dae1c3d2ce03a85432bdffb9f69e0f1babfcf5
zjh <zjh19940918@163.com>
2020-09-01 23:41:57 +0800

    first

---
+++ itrip-parent/src/main/java/com/zjh/itripparent/ItripParentApplication.java
@@ -0,0 +1,13 @@
+package com.zjh.itripparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItripParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItripParentApplication.class, args);
+    }
+
+}
---
+++ itrip-parent/src/test/java/com/zjh/itripparent/ItripParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjh.itripparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItripParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
